ede6d8f6a5aced5807340f23bf32794ca4018bdc
Carlos Enrique Guzmán Espinoza <carlose.guzmane62@hotmail.com>
2023-07-19 22:25:42 -0600

    :tada: Add: Project Structure

---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.scss';
+import { AcUnit } from '@material-ui/icons';
+
+function Home() {
+  return (
+    <div className="home">
+      <AcUnit />
+    </div>
+  );
+}
+
+export default Home;
