089fa766d4f4795e7df631c4527e55b03c46f48e
Carlos Enrique Guzmán Espinoza <carlose.guzmane62@hotmail.com>
2023-07-19 22:36:16 -0600

    :art: Add: Project Components

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './home.scss';
-import { AcUnit } from '@material-ui/icons';
+import Navbar from '../components/navbar/Navbar';
 
 function Home() {
   return (
     <div className="home">
-      <AcUnit />
+      <Navbar />
     </div>
   );
 }
